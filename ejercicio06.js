0c5029244970b8b4abca4150617d6e5b4074d410
Andres Gomez <andres_felipe1822@hotmail.com>
2023-03-07 21:34:37 -0500

    V1.1.0 ejercicio 6

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,8 +1,11 @@
 function calificacion (){
-    let a=parseFloat(document.getElementById('a').value);
-    let b=parseFloat(document.getElementById('b').value);
-    let c=parseFloat(document.getElementById('c').value);
-    let promedio=(a*0.55)+(b*0.3)+(c*0.15);
+    let parcial1=parseFloat(document.getElementById('parcial1').value);
+    let parcial2=parseFloat(document.getElementById('parcial2').value);
+    let parcial3=parseFloat(document.getElementById('parcial3').value);
+    let examenFinal=parseFloat(document.getElementById('examenFinal').value);
+    let trabajoFinal=parseFloat(document.getElementById('trabajoFinal').value);
+    let parcial =(parcial1+parcial2+parcial3)/3;
+    let promedio=(parcial*0.55)+(examenFinal*0.3)+(trabajoFinal*0.15);
     let decimal = promedio.toFixed(2);
     document.getElementById("nota").innerHTML=decimal;
     return decimal;
